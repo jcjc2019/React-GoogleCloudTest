64c844a8a65b4f2c01abd63ed280fbe0d12c7061
JingChen <jingchenjc2019@gmail.com>
2019-05-16 16:25:16 -0500

    google cloud test.

---
+++ public/precache-manifest.49b59a24eb49f172b4c431d78ea5221c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c10a190ff270a5a65913a3093d4cbfa5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9229063590a17999bb3d",
+    "url": "/static/js/2.00029d87.chunk.js"
+  },
+  {
+    "revision": "0d085cc8be1aaa0cedd7",
+    "url": "/static/js/main.40fb0d0f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
